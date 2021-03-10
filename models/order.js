dd08b5e03f9f2178532c0db21c378926e97b14a9
Stephen Crespo <stephencrespo9@gmail.com>
2021-03-10 16:36:48 -0500

    Add Order model/schema

---
+++ models/order.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  order_number: {
+    type: String,
+    required: true
+  },
+  json: {
+    type: String,
+    required: true
+  },
+}, { timestamps: true });
+
+module.exports = mongoose.model("Order", schema);
