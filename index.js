49b13053af2b583b6df4e12242036971cbfdfd9c
Stephen Crespo <stephencrespo9@gmail.com>
2021-03-24 14:50:52 -0400

    Clean up unused code

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
-// const error    = require('./middleware/error');
-// const AdminBro = require('admin-bro');
-// const AdminBroExpress = require('@admin-bro/express');
-// const AdminBroSequelize = require('@admin-bro/sequelize');
 const express  = require('express');
 const helmet  = require('helmet');
 const bodyParser = require('body-parser');
 // const Order = require('./models/order');
 // const User = require('./models/user');
 // const routes = require('./routes');
-
-//AdminBro.registerAdapter(AdminBroSequelize);
 
 
 //  Objection/Knex
@@ -54,66 +48,7 @@ createSchema();
 
 
 const app = express();
-
-// const resourceParent = {
-//   name: 'Resources',
-//   icon: 'Model',
-// }
-// 
-// const adminBro = new AdminBro ({
-//   resources: [
-//     { resource: Order, options: { parent: resourceParent } },
-//     {
-//       resource: User,
-//       options:  {
-//         parent: resourceParent
-//       },
-//       properties: {
-//         encrypted_password: {
-//           isVisible: false
-//         },
-//         password: {
-//           type: 'string',
-//           isVisible: {
-//             list: false, edit: true, filter: false, show: false
-//           }
-//         }
-//       },
-//       actions: {
-//         new: {
-//           before: async (request) => {
-//             if(request.payload.password) {
-//               request.payload = {
-//                 ...request.payload,
-//                 encryptedPassword: await bcrypt.hash(request.payload.password, 10),
-//                 password: undefined,
-//               }
-//             }
-//             return request
-//           }
-//         }
-//       }
-//     }
-//   ],
-//   rootPath: '/admin',
-// })
-// const router = AdminBroExpress.buildRouter (adminBro)
-// const router = AdminBroExpress.buildAuthenticatedRouter(adminBro, {
-//   authenticate: async (email, password) => {
-//     const user = await User.findOne({ email })
-//     if (user) {
-//       return user;
-//       const matched = await bcrypt.compare(password, user.encryptedPassword)
-//       if (matched) {
-//         return user
-//       }
-//     }
-//     return false
-//   },
-//   cookiePassword: 'some-secret-password-used-to-secure-cookie',
-// })
 app.use(helmet());
-// app.use(adminBro.options.rootPath, router);
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
@@ -182,8 +117,4 @@ app.get('/', (request, response) => {
 });
 // XXXXXXXXXXXXXXXXXXX
 
-
-// Error catching
-//app.use(error);
 app.listen(process.env.PORT, () => console.log(`Listening on port ${process.env.PORT}...`));
-// app.listen(process.env.PORT, () => console.log('AdminBro is under /admin'));
