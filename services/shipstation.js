777c88adbab769469cf89263be9a35ddcac098e1
Stephen Crespo <stephencrespo9@gmail.com>
2021-03-25 14:10:49 -0400

    Update shipstation service to reflect XML format

--- services/shipstation.js
+++ services/shipstation.js
@@ -14,75 +14,77 @@ function formatOrder(order) {
   orderJson = JSON.parse(order.json);
   return {
     Order: [
-      { orderNumber: order.number },
+      { OrderNumber: order.number },
       // orderKey: order.orderKey TODO
-      { orderDate: orderJson['processed_at'] },
-      { paymentDate: orderJson['processed_at'] },
-      { orderStatus: 'awaiting_shipment' }, // TODO: map this to shopify fulfillment status
-      { customerEmail: orderJson['email'] },
-      { billTo: addressFromJson(orderJson['billing_address']) },
-      { shipTo: addressFromJson(orderJson['shipping_address']) },
-      { items: orderItemFromJson(orderJson['line_items']) },
-      { amountPaid: '' },
-      { taxAmount: '' },
-      { shippingAmount: '' },
-      { customerNotes: '' },
-      { internalNotes: '' },
-      { gift: '' },
-      { giftMessage: '' },
-      { paymentMethod: '' },
-      { paymentMethod: '' },
-      { requestedShippingService: '' },
-      { carrierCode: '' },
-      { serviceCode: '' },
-      { packageCode: '' },
-      { confirmation: '' },
-      { weight: {} },
-      { dimensions: {} },
+      { OrderDate: orderJson['processed_at'] },
+      { PaymentDate: orderJson['processed_at'] },
+      { OrderStatus: 'awaiting_shipment' }, // TODO: map this to shopify fulfillment status
+      { CustomerEmail: orderJson['email'] },
+      { BillTo: addressFromJson(orderJson['billing_address']) },
+      { ShipTo: addressFromJson(orderJson['shipping_address']) },
+      { Items: orderItemsFromJson(orderJson['line_items']) },
+      { AmountPaid: '' },
+      { TaxAmount: '' },
+      { ShippingAmount: '' },
+      { CustomerNotes: '' },
+      { InternalNotes: '' },
+      { Gift: '' },
+      { GiftMessage: '' },
+      { PaymentMethod: '' },
+      { PaymentMethod: '' },
+      { RequestedShippingService: '' },
+      { CarrierCode: '' },
+      { ServiceCode: '' },
+      { PackageCode: '' },
+      { Confirmation: '' },
+      { Weight: {} },
+      { Dimensions: {} },
     ]
   };
 }
 
 function addressFromJson(json) {
-  if (json === undefined) { return {}; }
+  if (json === undefined) { return []; }
 
-  return {
-    name:       json['name'],
-    company:    json['company'],
-    street1:    json['address1'],
-    street2:    json['address2'],
-    // street3: json['street3'],
-    city:       json['city'],
-    state:      json['province'],
-    postalCode: json['zip'],
-    country:    json['country_code'],
-    phone:      json['phone'],
-    // residential: json['residential'],
-    // addressVerified: json['addressVerified'],
-  };
+  return [
+    { Name:       json['name'] },
+    { Company:    json['company'] },
+    { Street1:    json['address1'] },
+    { Street2:    json['address2'] },
+    // { street3: json['street3'] },
+    { City:       json['city'] },
+    { State:      json['province'] },
+    { PostalCode: json['zip'] },
+    { Country:    json['country_code'] },
+    { Phone:      json['phone'] },
+    // { residential: json['residential'] },
+    // { addressVerified: json['addressVerified'] },
+  ];
 }
 
 function orderItemsFromJson(itemsJson) {
-  itemsJson.map((itemJson) => { orderItemFromJson(itemJson); });
+  return itemsJson.map((itemJson) => orderItemFromJson(itemJson));
 }
 
 function orderItemFromJson(itemJson) {
   return {
-    lineItemKey: itemJson['id'],
-    sku: itemJson['sku'],
-    name: itemJson['title'],
-    // imageUrl: itemJson['id'],
-    weight: itemJson['grams'],
-    quantity: itemJson['quantity'],
-    unitPrice: itemJson['price'],
-    // taxAmount: itemJson['id'], // TODO
-    // shippingAmount: itemJson['id'], // TODO needed?
-    // warehouseLocation: itemJson['id'], // TODO needed?
-    // options: itemJson['id'], // TODO needed?
-    productId: itemJson['product_id'],
-    // fulfillmentSku: itemJson['id'], // TODO needed?
-    // adjustment: itemJson['id'], // TODO needed?
-    // upc: itemJson['id'], // TODO needed?
+    Item: [
+      { LineItemKey: itemJson['id'] },
+      { Sku: itemJson['sku'] },
+      { Name: itemJson['title'] },
+      // { imageUrl: itemJson['id'] },
+      { Weight: itemJson['grams'] },
+      { Quantity: itemJson['quantity'] },
+      { UnitPrice: itemJson['price'] },
+      // { taxAmount: itemJson['id'] }, // TODO
+      // { shippingAmount: itemJson['id' },, // TODO needed?
+      // { warehouseLocation: itemJson['id' },, // TODO needed?
+      // { options: itemJson['id' },, // TODO needed?
+      { ProductId: itemJson['product_id'] },
+      // { fulfillmentSku: itemJson['id' },, // TODO needed?
+      // { adjustment: itemJson['id' },, // TODO needed?
+      // { upc: itemJson['id' },, // TODO needed?
+    ]
   };
 }
 
